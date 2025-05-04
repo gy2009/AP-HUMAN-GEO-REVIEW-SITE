ed31a6b42c9052744ee12903345fe017bcda4368
gy2009 <gayatri.cheerful@gmail.com>
2025-05-03 20:35:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("mcq-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("MCQ answers submitted!");
+});
+
+document.getElementById("free-response-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Free response answers submitted!");
+});
